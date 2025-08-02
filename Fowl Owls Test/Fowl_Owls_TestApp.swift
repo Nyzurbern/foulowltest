243c5fb3ab737ab26603f09c9e3404ffb0ad9b83
Nyzurbern <Hrithik_tec@outlook.sg>
2025-08-02 09:51:01 +0800

    Initial Commit

---
+++ Fowl Owls Test/Fowl_Owls_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fowl_Owls_TestApp.swift
+//  Fowl Owls Test
+//
+//  Created by T Krobot on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Fowl_Owls_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
