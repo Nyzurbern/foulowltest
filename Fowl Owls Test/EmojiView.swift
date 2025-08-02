e172b5c9c3e136483c29f60997c8598aea47ec6f
Khoo Zi Qi <qiqicool7788@gmail.com>
2025-08-02 10:26:55 +0800

    av lotta chanfges

--- Fowl Owls Test/EmojiView.swift
+++ Fowl Owls Test/EmojiView.swift
@@ -2,6 +2,20 @@
 //  EmojiView.swift
 //  Fowl Owls Test
 //
-//  Created by T Krobot on 2/8/25.
+//  Created by Zi Qi on 2/8/25 to add emojis
 //
 
+/// Reusable emoji decorator
+import SwiftUI
+
+struct EmojiView: View {
+    let symbol: String
+    let size: CGFloat
+    let x: CGFloat
+    let y: CGFloat
+    var body: some View {
+        Text(symbol)
+            .font(.system(size: size))
+            .offset(x: x, y: y)
+    }
+}
